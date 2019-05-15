d02a10397cae90eb03a0f1a0709d757bdfe7c77f
Julio Londono <juliolondonogarcia@gmail.com>
2019-05-15 14:17:06 +0000

    first commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			clickedLight: null
+		};
+	}
+
+	render() {
+		let redExtraClass = "";
+		if (this.state.clickedLight == "red") redExtraClass = "selected";
+
+		let yellowExtraClass = "";
+		if (this.state.clickedLight == "yellow") yellowExtraClass = "selected";
+
+		let greenExtraClass = "";
+		if (this.state.clickedLight == "green") greenExtraClass = "selected";
+
+		return (
+			<div>
+				<div id="trafficTop" />
+				<div id="container">
+					<div
+						className={"light red " + redExtraClass}
+						onClick={() => this.setState({ clickedLight: "red" })}
+					/>
+
+					<div
+						className={"light yellow " + yellowExtraClass}
+						onClick={() =>
+							this.setState({ clickedLight: "yellow" })
+						}
+					/>
+
+					<div
+						className={"light green " + greenExtraClass}
+						onClick={() => this.setState({ clickedLight: "green" })}
+					/>
+				</div>
+			</div>
+		);
+	}
+}
